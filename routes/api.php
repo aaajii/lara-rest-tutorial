32cb125aa973227c59b2e433250c5f185261e601
Aji <jimuel.banawan@rococoglobaltech.com>
2020-07-16 13:57:38 +0800

    Laravel tutorial

--- routes/api.php
+++ routes/api.php
@@ -17,3 +17,20 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+// localhost:8000/api/v1/activities = List all activities [POST, GET, PATCH, DELETE]
+// localhost:8000/api/v1/activities/id/items = List all items under activity [POST, GET, PATCH, DELETE]
+
+Route::prefix('/v1')->group(function () {
+    Route::post('activities', 'ActivitiesController@store');
+    Route::post('activities/{activity_id}/items', 'ActivitiesController@storeLists');
+    
+    Route::get('activities', 'ActivitiesController@show');
+    Route::get('activities/{activity_id}', 'ActivitiesController@getActivityById');
+    
+    Route::patch('activities/{activity_id}', 'ActivitiesController@activityUpdate');
+    Route::patch('activities/{activity_id}/items/{item_id}', 'ActivitiesController@itemUpdate');
+
+    Route::delete('activities/{activity_id}', 'ActivitiesController@activityDestroy');
+    Route::delete('activities/{activity_id}/items/{item_id}', 'ActivitiesController@activityItemDestroy');
+});
